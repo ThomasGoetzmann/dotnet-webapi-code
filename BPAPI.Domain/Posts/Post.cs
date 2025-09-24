a9bc65bd3a1067bde821dfbbbdc4cd123639627c
Thomas G <9164479+ThomasGoetzmann@users.noreply.github.com>
2025-09-24 15:39:36 +0200

    remove BPAPI and add bruno collection

--- BPAPI.Domain/Posts/Post.cs
+++
@@ -1,3 +0,0 @@
-namespace BPAPI.Domain.Posts;
-
-public record Post(int Id, int UserId, string Title, string Body);
--- BPAPI.Domain/Todos/Todo.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace BPAPI.Domain.Todos;
-
-public record Todo(Guid Id, string Text, bool IsDone);
